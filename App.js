0eda48b2d5aa7b27407ebd5a699cfa8e17f535c9
Mthssss <matheus.monteiro1b1@gmail.com>
2024-04-11 10:46:50 -0300

    Formulario 3b1

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './SRC/Components/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Aplicativo IMC</Text>
       <StatusBar style="auto" />
+      <Main/>
     </View>
   );
 }
